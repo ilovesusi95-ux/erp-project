5fbf978ae484f0bf7e4028af4ec9dadc40d5de8b
ilovesusi95-ux <ilovesusi95@gmail.com>
2026-05-12 09:45:09 +0800

    在后台注册 Supplier 模型

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Supplier
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'sku', 'price', 'stock')
     search_fields = ('name', 'sku')
+
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ('name', 'contact_person', 'phone', 'email')
+    search_fields = ('name', 'contact_person', 'phone')
