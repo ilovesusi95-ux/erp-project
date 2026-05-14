78832dbc256849a88fcbebf1ec23bcd1ec746938
ilovesusi95-ux <ilovesusi95@gmail.com>
2026-05-14 15:36:33 +0800

    fix: 允许所有主机访问 (开发环境) - 修复 ALLOWED_HOSTS 导致的 0.0.0.0 错误

--- erp/settings.py
+++ erp/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-hc=ui_pps!ss$lqd2sv#bpjjk_2go+np_5ggt13tcd&95whz^k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -126,4 +126,4 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.models.BigAutoField'
